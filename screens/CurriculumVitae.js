d012c35340ed65c5436135f40326e7fd236f72a2
phuongvt24 <101643872+phuongvt24@users.noreply.github.com>
2023-01-18 15:42:48 +0700

    commit

---
+++ screens/CurriculumVitae.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+export default class CurriculumVitae extends Component {
+    render() {
+        return (
+            <View>
+                <Text> thajkda ajdaksjk ahsdkjashd ádkja đáhaksjd adasjdklas</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({});
